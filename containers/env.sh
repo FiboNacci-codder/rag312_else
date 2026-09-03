b9d5acd67565d5becb4f1491e2f6eb4220cb9286
FiboNacci-codder <atauchimamani@gmail.com>
2026-09-03 16:21:40 -0500

    fix: env.sh

--- containers/env.sh
+++ containers/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export VLLM_SIF="$HOME/rag312/containers/vllm-v0271.sif"
-export HF_HOME="$HOME/rag312/modelos/hf_cache"
-export FASTEMBED_CACHE_DIR="$HOME/rag312/modelos/fastembed_cache"
+export VLLM_SIF="$HOME/rag312_else/containers/vllm-v0271.sif"
+export HF_HOME="$HOME/rag312_else/modelos/hf_cache"
+export FASTEMBED_CACHE_DIR="$HOME/rag312_else/modelos/fastembed_cache"
